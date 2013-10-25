834953a605930c1c1eeeec81830f33791b5135df
srbz <srbz@hotmail.de>
2013-10-25 23:12:37 +0200

    second commit

---
+++ files/lib/page/ServerOverviewPage.class.php
@@ -0,0 +1,18 @@
+<?php
+// wcf imports
+require_once(WCF_DIR.'lib/page/AbstractPage.class.php');
+
+/**
+ * Just an example page
+ *
+ * @author    zerocool
+ * @copyright    2013 sexygaming.de
+ * @package    de.sexygaming.tsviewer
+ * @license    insert it here
+ */
+class ServerOverviewPage extends AbstractPage {
+    // ServerOverview.tpl
+    public $templateName = 'ServerOverview';
+    
+}
+?>
