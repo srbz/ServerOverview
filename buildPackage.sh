f6501e9593336bde9040729078970fdcfa2e2dea
Christoph Kretzschmar <ck@robhost.de>
2013-10-28 19:30:29 +0100

    Pull GameQ as Git Submodule

--- buildPackage.sh
+++ buildPackage.sh
@@ -4,9 +4,23 @@ if [ -d build ]; then
     rm -R build 
 fi
 
+# Create clean build directory
 mkdir build
-cd src
-cd files && tar -cf ../../build/files.tar * && cd ..
-cd templates && tar -cf ../../build/templates.tar * && cd ..
-cp *.xml ../build/ && cd ..
-cd build && tar -cf installPackage.tar * && cd ..
+
+# copy all src files into build
+cp -R src/* build/
+
+# ./build - Link Librarys into build dir
+cd build && ln -s ../../../../../lib/GameQ files/lib/page/util/
+# ./build/files - create files.tar
+cd files && tar -chf ../files.tar * && cd ..
+# ./build/templates - create templates.tar
+cd templates && tar -chf ../templates.tar * && cd ..
+
+# ./build/tmp
+mkdir tmp && cd tmp
+cp ../*.{xml,tar} .
+
+# ./build - create installPackage.tar & back to ./
+tar -chf ../installPackage.tar * && cd .. && rm -R tmp && cd ..
+
